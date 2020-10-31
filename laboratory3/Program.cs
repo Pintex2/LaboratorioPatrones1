7fe7cb277879dcafebdd6b42ae815d4dfdd5f549
Andre Pinto <42154799+Pintex2@users.noreply.github.com>
2020-10-30 22:19:11 -0400

    Update Program.cs

--- laboratory3/Program.cs
+++ laboratory3/Program.cs
@@ -10,10 +10,10 @@ namespace Games_Factory
             Game_Store store = new Concrete_Game_Store();
 
             IConsole ps5 = store.GetConsole("Ps5");
-            ps5.sell_console("Benito", 500);
+            ps5.sell_console(5723,"Benito", 500);
 
             IConsole xbox = store.GetConsole("Xbox");
-            xbox.sell_console("juanito", 400);
+            xbox.sell_console(5231,"juanito", 400);
 
             Console.ReadKey();
         }
