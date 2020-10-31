4ea225110ef81ffa8698a09679e475ff4d013aab
Andre Pinto <42154799+Pintex2@users.noreply.github.com>
2020-10-30 22:21:37 -0400

    Update Ps5.cs

--- laboratory3/Ps5.cs
+++ laboratory3/Ps5.cs
@@ -4,10 +4,10 @@ namespace Games_Factory
 {
     class Ps5 : IConsole
     {
-        public void sell_console(string name, int price)
+        public void sell_console(int consoleCode,string name, int price)
         {
 
-            Console.WriteLine("Ps5 selled to : " + name + " and price: " + price.ToString() + "$");
+            Console.WriteLine("Console code:"+consoleCode.ToString()+"Ps5 selled to : " + name + " and price: " + price.ToString() + "$");
         }
     }
 }
